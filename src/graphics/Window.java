aedc7db12f70f2182eeb02dda4f945b9e9b1b27c
vague315 <vague315@gmail.com>
2015-03-16 20:21:54 -0500

    Added basic funtionality

--- src/graphics/Window.java
+++ src/graphics/Window.java
@@ -1,5 +1,129 @@
 package graphics;
 
-public class Window {
+import static org.lwjgl.glfw.GLFW.GLFW_CONTEXT_VERSION_MAJOR;
+import static org.lwjgl.glfw.GLFW.GLFW_CONTEXT_VERSION_MINOR;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_ESCAPE;
+import static org.lwjgl.glfw.GLFW.GLFW_OPENGL_CORE_PROFILE;
+import static org.lwjgl.glfw.GLFW.GLFW_OPENGL_FORWARD_COMPAT;
+import static org.lwjgl.glfw.GLFW.GLFW_OPENGL_PROFILE;
+import static org.lwjgl.glfw.GLFW.GLFW_PRESS;
+import static org.lwjgl.glfw.GLFW.GLFW_RESIZABLE;
+import static org.lwjgl.glfw.GLFW.glfwCreateWindow;
+import static org.lwjgl.glfw.GLFW.glfwDefaultWindowHints;
+import static org.lwjgl.glfw.GLFW.glfwDestroyWindow;
+import static org.lwjgl.glfw.GLFW.glfwGetPrimaryMonitor;
+import static org.lwjgl.glfw.GLFW.glfwGetVideoMode;
+import static org.lwjgl.glfw.GLFW.glfwMakeContextCurrent;
+import static org.lwjgl.glfw.GLFW.glfwPollEvents;
+import static org.lwjgl.glfw.GLFW.glfwSetCursorPosCallback;
+import static org.lwjgl.glfw.GLFW.glfwSetKeyCallback;
+import static org.lwjgl.glfw.GLFW.glfwSetWindowPos;
+import static org.lwjgl.glfw.GLFW.glfwSetWindowShouldClose;
+import static org.lwjgl.glfw.GLFW.glfwSetWindowTitle;
+import static org.lwjgl.glfw.GLFW.glfwSwapBuffers;
+import static org.lwjgl.glfw.GLFW.glfwSwapInterval;
+import static org.lwjgl.glfw.GLFW.glfwTerminate;
+import static org.lwjgl.glfw.GLFW.glfwWindowHint;
+import static org.lwjgl.glfw.GLFW.glfwWindowShouldClose;
+import static org.lwjgl.opengl.GL11.GL_FALSE;
+import static org.lwjgl.opengl.GL11.GL_TRUE;
+import static org.lwjgl.system.MemoryUtil.NULL;
 
+import java.nio.ByteBuffer;
+
+import org.lwjgl.glfw.GLFWCursorPosCallback;
+import org.lwjgl.glfw.GLFWKeyCallback;
+import org.lwjgl.glfw.GLFWvidmode;
+import org.lwjgl.opengl.GLContext;
+
+public class Window {
+	private final long handle;
+	private boolean vsync;
+	
+    private final GLFWKeyCallback keyCallback;
+    private final GLFWCursorPosCallback mousePosCallback;    
+
+    public Window(int width, int height, CharSequence title, boolean vsync) {
+        this.vsync = vsync;
+
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+        glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+        // Create window and its opengl context
+        handle = glfwCreateWindow(width, height, title, NULL, NULL);
+        if (handle == NULL) {
+            glfwTerminate();
+            throw new RuntimeException("Failed to create the GLFW window!");
+        }
+
+        // Center window
+        ByteBuffer vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+        glfwSetWindowPos(handle,
+                (GLFWvidmode.width(vidmode) - width) / 2,
+                (GLFWvidmode.height(vidmode) - height) / 2
+        );
+
+        glfwMakeContextCurrent(handle);
+        GLContext.createFromCurrent();
+
+        // Enable vsync
+        if (vsync) {
+            glfwSwapInterval(1);
+        }
+
+        // Set callbacks
+        glfwSetKeyCallback(handle, keyCallback = new GLFWKeyCallback() {
+            @Override
+            public void invoke(long window, int key, int scancode, int action, int mods) {
+                if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+                    glfwSetWindowShouldClose(window, GL_TRUE);
+                }
+            }
+        });
+        
+        glfwSetCursorPosCallback(handle, mousePosCallback = new GLFWCursorPosCallback(){
+        	@Override
+        	public void invoke(long window, double xpos, double ypos) {
+        		System.out.println("xpos " + xpos + " ypos " + ypos);
+        	}
+        });
+
+    }
+
+    public boolean isClosing() {
+        return glfwWindowShouldClose(handle) == GL_TRUE;
+    }
+
+    public void setTitle(CharSequence title) {
+        glfwSetWindowTitle(handle, title);
+    }
+
+    public void update() {
+        glfwSwapBuffers(handle);
+        glfwPollEvents();
+    }
+
+    public void destroy() {
+        glfwDestroyWindow(handle);
+        keyCallback.release();
+        mousePosCallback.release();
+    }
+
+    public void setVSync(boolean vsync) {
+        this.vsync = vsync;
+        if (vsync) {
+            glfwSwapInterval(1);
+        } else {
+            glfwSwapInterval(0);
+        }
+    }
+
+    public boolean isVSyncEnabled() {
+        return this.vsync;
+    }
 }
